93e5164029fd4c3df7b033d2cbca1e1253d4e78c
TychiqueMas <140072006+TychiqueMas@users.noreply.github.com>
2023-09-06 16:01:31 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,143 @@
+// Votre console affichera beaucoup d'erreurs c'est tout à fait normal si vous suivez l'ordre synchrone des exercices à la fin vous ne devriez plus avoir d'erreur.
+
+// Exercice 1
+const firstname = "Tychique"
+const lastname = "Masibu"
+
+// Ne touchez pas à ceci
+document.querySelector("h1").textContent = `Bonjour ${firstname} ${lastname}`;
+
+// Exercice 2
+let age = 60
+
+// Ne touchez pas à ceci
+document.querySelector(
+  "p"
+).textContent = `Tu t'appelles donc ${firstname} et tu as ${age} ans 😊 si mes calculs sont justes tu es né en ${2023 - age
+}`;
+// Exercice 3
+let year = 2023
+let birthYear = year - age
+
+
+// Exercice 4
+let user = [firstname, lastname, age, year, birthYear]
+
+// Ne touchez pas à ceci
+user.forEach((infoUser) => {
+  switch (infoUser) {
+    case firstname:
+      document.getElementById("firstname").textContent = `Salut ${firstname} !`;
+      break;
+    case lastname:
+      document.getElementById(
+        "lastname"
+      ).textContent = `Bonjour Monsieur/Madame ${lastname} !`;
+      break;
+    case age:
+      document.getElementById(
+        "age"
+      ).textContent = `Quel âge as-tu ? ${age} ans`;
+      break;
+    case year:
+      document.getElementById(
+        "year"
+      ).textContent = `On est toujours en ${year}`;
+      break;
+    case birthYear:
+      document.getElementById(
+        "birthYear"
+      ).textContent = `Tu es donc né en ${birthYear}`;
+      break;
+    default:
+      console.log("Oups il y a une erreur");
+      break;
+  }
+});
+// Exercice 5
+let backgroundColor = "blue"
+backgroundColor = "gray"
+
+// Ne touchez pas à ceci
+document.body.style.backgroundColor = backgroundColor;
+// Insérez votre réassignation de backgroundColor ici
+
+document.body.style.backgroundColor = backgroundColor;
+
+// Exercice 6
+const car = {
+  brand: "Mercedes",
+  model: "Class G",
+  yearConstruct: 2023,
+  color: "Black",
+  image: "https://i.pinimg.com/736x/e5/75/fd/e575fdce8da3bce34e0345eb7220d1ad.jpg"
+};
+
+// Ne touchez pas à ceci
+document.getElementById(
+  "firstItem"
+).textContent = `La marque de ta voiture : ${car.brand}`;
+document.getElementById(
+  "secondItem"
+).textContent = `Le modèle de ta voiture : ${car.model}`;
+document.getElementById(
+  "thirdItem"
+).textContent = `Ta voiture a été contruite en : ${car.yearConstruct}`;
+document.getElementById(
+  "fourthItem"
+).textContent = `Ta voiture est de couleur : ${car.color}`;
+document.getElementById("car").src = car.image;
+
+// Exercice 7
+const buttons = document.querySelectorAll("button");
+const cat = document.getElementById("cat");
+
+// Ne touche pas à ceci
+const catURL = [
+  "https://s1.1zoom.me/big0/812/Cats_Glance_Kittens_558640_1280x720.jpg",
+  "https://img3.wallspic.com/crops/4/8/6/4/94684/94684-chat_siberien-chat_siamois-chaton-chat_persan-chat-1280x720.jpg",
+  "https://chatfaitdubien.fr/wp-content/uploads/2016/09/chaton.jpg",
+];
+buttons.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    switch (btn.textContent) {
+      case "Photo 1":
+        cat.src = catURL[0];
+        break;
+      case "Photo 2":
+        cat.src = catURL[1];
+        break;
+      case "Photo 3":
+        cat.src = catURL[2];
+        break;
+      default:
+        console.log("Erreur");
+        break;
+    }
+  });
+});
+
+// Exercice 8
+const output = getElementById("output")
+
+// Exercice 9
+// Insérez votre code ici
+
+// Exercice 10
+// Ne touchez pas au tableau
+const icons = [
+  "cat-solid",
+  "dog-solid",
+  "dragon-solid",
+  "fish-solid",
+  "hippo-solid",
+  "horse-solid",
+  "otter-solid",
+  "paw-solid",
+  "shrimp-solid",
+];
+
+// Insérez votre code ici
+
+// Exercice 11 : Optionnel
+// Insérez votre code ici
